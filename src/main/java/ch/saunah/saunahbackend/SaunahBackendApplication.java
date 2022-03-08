436bf6fb8edb7cc2df1dde48a361a33fd35f8d61
Simon Schuhmacher <schuhsim@students.zhaw.ch>
2022-03-08 15:34:40 +0100

    add spring boot initial setup

---
+++ src/main/java/ch/saunah/saunahbackend/SaunahBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.saunah.saunahbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaunahBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaunahBackendApplication.class, args);
+	}
+
+}
