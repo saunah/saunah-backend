292099ae5db2d2d936165c7194fd45ceb3a2ea93
Simon Schuhmacher <schuhsim@students.zhaw.ch>
2022-03-08 22:59:02 +0100

    allow cors from localhost:8080

--- src/main/java/ch/saunah/saunahbackend/SaunahBackendApplication.java
+++ src/main/java/ch/saunah/saunahbackend/SaunahBackendApplication.java
@@ -2,12 +2,24 @@ package ch.saunah.saunahbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SaunahBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SaunahBackendApplication.class, args);
 	}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
 }
