85cb7797504514ba17f2b9c2b4e6477d21e52d6f
Simon Schuhmacher <schuhsim@students.zhaw.ch>
2022-03-08 21:48:04 +0100

    add sample greeting controller

---
+++ src/main/java/ch/saunah/saunahbackend/GreetingController.java
@@ -0,0 +1,19 @@
+package ch.saunah.saunahbackend;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping(value = {"/", "/{name}"})
+    public Greeting greeting(@PathVariable(value = "name", required = false) Optional<String> name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name.orElse("World")));
+    }
+}
