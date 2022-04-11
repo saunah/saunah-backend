c6bef96e153852e63a02b88ff1093bcb51c00f93
Marcel Scherrer <schmar01@students.zhaw.ch>
2022-04-11 21:23:02 +0200

    create all required fields for DB

--- src/main/java/ch/saunah/saunahbackend/SaunaController.java
+++ src/main/java/ch/saunah/saunahbackend/SaunaController.java
@@ -1,7 +1,6 @@
 package ch.saunah.saunahbackend;
 
-import ch.saunah.saunahbackend.model.SaunaType;
-import ch.saunah.saunahbackend.model.User;
+import ch.saunah.saunahbackend.model.Sauna;
 import ch.saunah.saunahbackend.model.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-public class SaunahTypeController {
+public class SaunaController {
     private final AtomicLong counter = new AtomicLong();
 
     // TODO change to SaunaTypeRepository
@@ -22,15 +21,23 @@ public class SaunahTypeController {
     @Operation(description = "Allows adding a new Sauna type.")
     @PostMapping(path = "user/add") // Map ONLY POST Requests
     public @ResponseBody
-    String addSaunaType(@RequestParam("sauna_name") String saunaName,
-                        @RequestParam("sauna_description") String saunaDescription,
-                        @RequestParam("sauna_Type") boolean isMobile,
-                        @RequestParam("sauna_Location") String saunaLocation) {
+    String addSaunaType(@RequestParam("name") String name,
+                        @RequestParam("description") String description,
+                        @RequestParam("picture") boolean picture,
+                        @RequestParam("type") boolean isMobile,
+                        @RequestParam("preis") int preis,
+                        @RequestParam("maxTemp") int maxTemp,
+                        @RequestParam("numberOfPeople") int numberOfPeople,
+                        @RequestParam("location") String location,
+                        @RequestParam("street") String street,
+                        @RequestParam("plz") int plz) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
-        SaunaType saunaType = new SaunaType(saunaName, saunaDescription, isMobile,saunaLocation);
-        //userRepository.save(saunaType);
-        return String.format("The sauna was saved with id %s", saunaType.getSaunaId());
+        Sauna saunaType = new Sauna(name, description, picture, isMobile, preis, maxTemp, numberOfPeople,
+            location, street, plz) {
+            //userRepository.save(saunaType);
+        };
+        return String.format("The sauna was saved with id %s",saunaType.getId());
     }
 
     // TODO Implement??
---
+++ src/main/java/ch/saunah/saunahbackend/model/Sauna.java
@@ -0,0 +1,101 @@
+package ch.saunah.saunahbackend.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity(name = "sauna")
+public class Sauna {
+
+    @Id
+    @Column(name="id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    // TODO How to save pictures in DB?
+    @Column(name = "picture", nullable = false)
+    private boolean picture;
+
+    @Column(name = "type", nullable = false)
+    private boolean isMobile;
+
+    @Column(name = "preis", nullable = false)
+    private int preis;
+
+    @Column(name = "max_temp", nullable = false)
+    private int maxTemp;
+
+    @Column(name = "numberOfPeople", nullable = false)
+    private int numberOfPeople;
+
+    @Column(name = "location", nullable = false)
+    private String location;
+
+    @Column(name = "street", nullable = false)
+    private String street;
+
+    @Column(name = "plz", nullable = false)
+    private int plz;
+
+    public Sauna(String name, String description, boolean picture, boolean isMobile,
+                int preis, int maxTemp, int numberOfPeople, String location, String street, int plz){
+        this.name = name;
+        this.description = description;
+        this.picture = picture;
+        this.isMobile = isMobile;
+        this.preis = preis;
+        this.maxTemp = maxTemp;
+        this.numberOfPeople = numberOfPeople;
+        this.location = location;
+        this.street = street;
+        this.plz = plz;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean getType() {
+        return isMobile;
+    }
+
+    public int getPreis() {
+        return preis;
+    }
+
+    public int getMaxTemp() { return maxTemp; }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public int getPlz() {
+        return plz;
+    }
+}
+
+
+
