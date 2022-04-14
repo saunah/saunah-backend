83406381d8c3155da4c68d5f34959843c44af0e4
Simon Schuhmacher <schuhsim@students.zhaw.ch>
2022-04-14 15:02:44 +0200

    add 50 % coverage for test

---
+++ src/main/java/ch/saunah/saunahbackend/CodeCoverageCheck.java
@@ -0,0 +1,23 @@
+package ch.saunah.saunahbackend;
+
+/**
+ * A sample class to test code coverage scan on sonarcloud
+ */
+public class CodeCoverageCheck {
+
+    /**
+     * Sample check class one
+     */
+    public int checkCoverageOne() {
+        return 1;
+    }
+
+
+    /**
+     * Sample check class two
+     * @param param1
+     */
+    public int checkCoverageTwo(int param1) {
+        return param1;
+    }
+}
---
+++ src/test/java/ch/saunah/saunahbackend/CheckCoverageTest.java
@@ -0,0 +1,18 @@
+package ch.saunah.saunahbackend;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CheckCoverageTest {
+    /**
+     * A sample dummy test to CI/CD.
+     */
+	@Test
+	void dummyTest() {
+        CodeCoverageCheck check = new CodeCoverageCheck();
+        assertEquals(1, check.checkCoverageOne() );
+	}
+}
