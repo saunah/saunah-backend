85124d6212888b6b4b521ad5e229aeb4aacd0442
Simon Schuhmacher <schuhsim@students.zhaw.ch>
2022-04-14 11:06:13 +0200

    add new class to test coverage scan

---
+++ src/main/java/ch/saunah/saunahbackend/CodeCoverageCheck.java
@@ -0,0 +1,23 @@
+package ch.saunah.saunahbackend;
+
+/**
+ * A sample class to test code coverage scan on sonarcloud
+ */
+public class CodeCoverageCheck {
+
+    /**
+     * Sample check class one
+     */
+    public void checkCoverageOne() {
+        System.out.println("Check coverage one called");
+    }
+
+
+    /**
+     * Sample check class two
+     * @param param1
+     */
+    public void checkCoverageTwo(int param1) {
+        System.out.println("Check coverage two called.");
+    }
+}
