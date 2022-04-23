095d480db32587c45d6a46d7d895ff916bbf318d
Marcel Scherrer <schmar01@students.zhaw.ch>
2022-04-23 23:15:06 +0200

    implement PriceBody

--- src/main/java/ch/saunah/saunahbackend/dto/PriceBody.java
+++ src/main/java/ch/saunah/saunahbackend/dto/PriceBody.java
@@ -1,4 +1,45 @@
 package ch.saunah.saunahbackend.dto;
 
 public class PriceBody {
+    private float baseRent;
+    private float transportService;
+    private float washService;
+    private float saunahImp;
+    private float deposit;
+    private float handTowel;
+    private float wood;
+    private float extras;
+
+    public float getBaseRent() { return baseRent; }
+
+    public float getTransportService() { return  transportService; }
+
+    public float getWashService() { return  washService; }
+
+    public float getSaunahImp() { return saunahImp; }
+
+    public float getDeposit() { return deposit; }
+
+    public float getHandTowel() { return handTowel; }
+
+    public float getWood() { return wood; }
+
+    public float getExtras() { return extras; }
+
+    public void setBaseRent(float baseRent) { this.baseRent = baseRent; }
+
+    public void setTransportService(float transportService) { this.transportService = transportService; }
+
+    public void setWashService(float washService) { this.washService = washService; }
+
+    public void setSaunahImp(float saunahImp) { this.saunahImp = saunahImp; }
+
+    public void setDeposit(float deposit) { this.deposit = deposit; }
+
+    public void setHandTowel(float handTowel) { this.handTowel = handTowel; }
+
+    public void setWood(float wood) { this.wood = wood; }
+
+    public void setExtras(float extras) { this.extras = extras;}
+
 }
