c07f35d03a70e9e0a6ae72ee998feacdb72b088e
cabernin <nino.cabernard@outlook.com>
2022-04-27 17:22:13 +0200

    #32 not used imports removed

--- src/main/java/ch/saunah/saunahbackend/model/SaunaBooking.java
+++ src/main/java/ch/saunah/saunahbackend/model/SaunaBooking.java
@@ -1,17 +1,11 @@
 package ch.saunah.saunahbackend.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Model of a sauna booking
