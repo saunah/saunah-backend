a77ac862a742767df541ba60b676d4878a4e5f89
utschu <arjuna1997@windowslive.com>
2022-05-03 23:29:02 +0200

    Simon's sini changes wieder zruggholt. Has bim mergeconflikt usversehe glöscht

--- src/test/java/ch/saunah/saunahbackend/service/UserServiceTest.java
+++ src/test/java/ch/saunah/saunahbackend/service/UserServiceTest.java
@@ -1,13 +1,14 @@
 package ch.saunah.saunahbackend.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-import ch.saunah.saunahbackend.SaunahBackendApplication;
-import ch.saunah.saunahbackend.dto.ResetPasswordBody;
-import ch.saunah.saunahbackend.dto.SignInBody;
-import ch.saunah.saunahbackend.dto.SignUpBody;
-import ch.saunah.saunahbackend.model.User;
-import ch.saunah.saunahbackend.model.UserRole;
-import ch.saunah.saunahbackend.repository.UserRepository;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,7 @@ import ch.saunah.saunahbackend.dto.SignUpBody;
 import ch.saunah.saunahbackend.model.User;
 import ch.saunah.saunahbackend.model.UserRole;
 import ch.saunah.saunahbackend.repository.UserRepository;
+import ch.saunah.saunahbackend.dto.ResetPasswordBody;
 
 /**
  * This class tests all user service methods.
