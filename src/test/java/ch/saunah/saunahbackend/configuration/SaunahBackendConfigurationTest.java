5c3a92699193d373144cb4c0897ae46c355f2829
Simon Schuhmacher <schuhsim@students.zhaw.ch>
2022-05-04 13:27:24 +0200

    add some more tests

--- src/test/java/ch/saunah/saunahbackend/configuration/SaunahBackendConfigurationTest.java
+++ src/test/java/ch/saunah/saunahbackend/configuration/SaunahBackendConfigurationTest.java
@@ -15,13 +15,32 @@ class SaunahBackendConfigurationTest {
     private static final String[] EXTRA_CORS = new String[]{"https://localhost:3000", "https://localhost:3001"};
 
     @Test
-    void testGetAllowedCors() {
+    void testExtraCorsSet() {
         String[] actualCors = SaunahBackendConfiguration.getAllowedCors(
             FRONTEND_URL, String.join(",", EXTRA_CORS));
 
         String[] expectedCors = Stream.concat(
             Stream.of(FRONTEND_URL), Arrays.stream(EXTRA_CORS))
             .toArray(String[]::new);
+
+        assertArrayEquals(expectedCors, actualCors);
+    }
+
+    @Test
+    void testExtraCorsEmpty() {
+        String[] actualCors = SaunahBackendConfiguration.getAllowedCors(
+            FRONTEND_URL, String.join(",", ""));
+
+        String[] expectedCors = new String[]{FRONTEND_URL};
+
+        assertArrayEquals(expectedCors, actualCors);
+    }
+
+    @Test
+    void testExtraCorsNull() {
+        String[] actualCors = SaunahBackendConfiguration.getAllowedCors(
+            FRONTEND_URL, null);
+        String[] expectedCors = new String[]{FRONTEND_URL};
 
         assertArrayEquals(expectedCors, actualCors);
     }
