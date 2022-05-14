fa20d77db1698f15d8d68996c5b1e67d462aa158
Marcel Scherrer <schmar01@students.zhaw.ch>
2022-05-14 09:31:18 +0200

    add fields for initialAdmin and isDeleted

--- src/main/java/ch/saunah/saunahbackend/model/User.java
+++ src/main/java/ch/saunah/saunahbackend/model/User.java
@@ -52,6 +52,12 @@ public class User {
     @Column(name = "resetpasswordhash")
     private String resetpasswordhash;
 
+    @Column(name = "initialAdmin")
+    private boolean initialAdmin;
+
+    @Column(name = "isDeleted")
+    private boolean isDeleted;
+
     /**
      * The default constructor for the user.
      */
@@ -155,4 +161,11 @@ public class User {
 
     public void setResetpasswordHash(String resetpassword_hash){this.resetpasswordhash = resetpassword_hash;}
 
+    public boolean getInitialAdmin() {return initialAdmin; }
+
+    public  void setInitialAdmin(boolean initialAdmin) { this.initialAdmin = initialAdmin;}
+
+    public boolean getIsDeleted() {return isDeleted; }
+
+    public void setIsDeleted(boolean isDeleted) { this.isDeleted = isDeleted;}
 }
